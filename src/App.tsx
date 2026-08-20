0314dce8c089be1b34c06ee1c2ed6b5f97a711b0
Maksym Bondarchuk <belkamax05@gmail.com>
2026-08-20 03:43:17 +0100

    add "back" link

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return (
-    <main>
-      <h1>belkamax05</h1>
-      <p>Coming soon.</p>
-    </main>
-  );
+	const [user, setUser] = useState({
+		name: "",
+	});
+
+	useEffect(() => {
+		fetch("https://api.github.com/users/belkamax05")
+			.then((res) => res.json())
+			.then((data) => setUser(data));
+	}, []);
+
+	return (
+		<main>
+			<h1>{user.name}</h1>
+			<p>Coming soon.</p>
+			<p>
+				Back to <a href="https://github.com/belkamax05">Github</a>
+			</p>
+		</main>
+	);
 }
